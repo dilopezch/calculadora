9a3d2683b800dbfa3a1dc767907875964e8e43d5
dilopezch <dilopezch@gmail.com>
2024-04-22 15:41:25 -0500

    feat: agrega operación al boton limpiar

--- js/script.js
+++ js/script.js
@@ -1,23 +1,56 @@
 let pantalla;
-let botones;
+let botones = [];
+let operaciones = [];
+let valor;
+let valor1, valor2;
 
-window.onload = function() {
+window.onload = cargarElementos;
+
+function cargarElementos() {
+    operaciones[0] = document.getElementById("btnLimpiar");
+    operaciones[1] = document.getElementById("btnPorcentaje"); 
+    operaciones[2] = document.getElementById("btnEliminar"); 
+    operaciones[3] = document.getElementById("btnDivision"); 
+    operaciones[4] = document.getElementById("btnMultiplicar"); 
+    operaciones[5] = document.getElementById("btnRestar"); 
+    operaciones[6] = document.getElementById("btnSumar"); 
+    operaciones[7] = document.getElementById("btnIgual"); 
+
     pantalla = document.getElementById("output-box");
-    botones = document.getElementsByClassName("btn");
+    botones = document.getElementsByClassName("btn-numbers");
     agregarEventos(botones);
     pantalla.value = "";
 }
 
-function agregarEventos(botones){
+function agregarEventos(){
     for (let i=0; i<botones.length; i++) {
-        botones[i].addEventListener("click", mostrarValorPantalla)
+        botones[i].addEventListener("click", mostrarNumeroPantalla)
+    }
+
+    for (let i=0; i<operaciones.length; i++){
+        operaciones[i].addEventListener("click", presionarTeclaOperacion)
     }
 }
 
-function mostrarValorPantalla() {
+function mostrarNumeroPantalla() {
     let valorBoton = this.innerHTML;
 
     if (pantalla.value.length<10) {
         pantalla.value += valorBoton;
     }
+}
+
+function reiniciarPantalla(){
+    valor = ""; 
+    pantalla.value = "";
+}
+
+function presionarTeclaOperacion(event){
+    switch(event.target.id) {
+        case "btnLimpiar": 
+            reiniciarPantalla();
+            pantalla.value = "";
+            break;
+    }
+
 }
